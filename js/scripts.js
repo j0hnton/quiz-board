09884acb5c4f1cfe4067538dd10f3cfb49f27cea
j0hnton <afrikiddo@gmail.com>
2019-02-24 23:52:49 +0300

    Change the layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
-  $("#quiz").submit(function(event){
-    event.preventDefault();
-    var answer1 = ($("radio[name=q1]").val());
-    var answer2 = ($("radio[name=q2]").val());
-    var answer3 = ($("radio[name=q3]").val());
-    var answer4 = ($("input:radio[name=q4]").val());
-    var answer5 = ($("input:radio[name=q5]").val());
+    $("button#start").click(function(){
+      $("#faded").toggle(3000);
+      $("#fade").toggle(1000);
+      $("#leave").toggle(1000);
+
+  });
+
+  $("#faded").submit(function(){
+
+    var answer1 = $("input:radio[name=q1]:checked").val();
+    var answer2 = $("input:radio[name=q2]:checked").val();
+    var answer3 = $("input:radio[name=q3]:checked").val();
+    var answer4 = $("input:radio[name=q4]:checked").val();
+    var answer5 = $("input:radio[name=q5]:checked").val();
 
     var number1=parseInt(answer1);
     var number2=parseInt(answer2);
@@ -13,16 +20,19 @@ $(document).ready(function(){
     var number4=parseInt(answer4);
     var number5=parseInt(answer5);
 
-    var display =((number1 + number2 + number3 + number4 + number5 )/50*100);
+    var show =((number1+number2+number3+number4+number5 )/50*100);
+alert(show);
 
-    $("#results").text(display+"%");
+if(show>=80){
+  alert("Excellent")
+
+ if((show<79)(show>50)){
+  alert("Fair")
+}
+else{
+  alert("Poor. RETAKE THE TEST")
+}
+}
+  event.preventDefault();
   });
 });
-
-$(document).ready(function(){
-  $("button#start").click(function(){
-    $("#faded").toggle(3000);
-    $("#fade").toggle(1000);
-    $("#leave").toggle(1000);
-  })
-})
